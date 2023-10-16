6e0bf6cee0b098f35ed89d8f590f0deb906e3a69
swarnim adarsh <swarnimadarsh.3579@gmail.com>
2023-10-16 12:04:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter, Routes, Route} from 'react-router-dom';
+import './components/style.css'
+import Navbar from './components/Navbar';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Home , About, Info, Upload, Contact, Project, sidebar} from './components/Navcomponents';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Project />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
